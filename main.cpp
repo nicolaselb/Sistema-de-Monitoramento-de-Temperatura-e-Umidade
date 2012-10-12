9179edf7567bddc29c6ac54c0c2dc640ce1f72bb
chris <chris@localhost>
2012-10-12 13:49:40 +0000

    Made thisi an HSV example

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "mbed.h"
 
-BusOut rgb(LED_RED,LED_GREEN,LED_BLUE);
+PwmOut r (LED_RED);
+PwmOut g (LED_GREEN);
+PwmOut b (LED_BLUE);
 
-int main() {
-    int i=0;   
+int main()
+{
+    r.period(0.001);
+    g.period(0.001);
+    b.period(0.001);
+
     while(1) {
-        rgb=i;
-        wait (0.25);
-        i++;            
-     }
+        for(float i = 0.0; i < 1.0 ; i += 0.001) {
+            float p = 3 * i;
+            r = 1.0 - ((p < 1.0) ? 1.0 - p : (p > 2.0) ? p - 2.0 : 0.0);
+            g = 1.0 - ((p < 1.0) ? p : (p > 2.0) ? 0.0 : 2.0 - p);
+            b = 1.0 - ((p < 1.0) ? 0.0 : (p > 2.0) ? 3.0 - p : p - 1.0);  ;  
+            wait (0.0025);
+        }
+    }
 }
