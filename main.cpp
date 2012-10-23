6d77f1148c5c8b3b323e03d78a67952a1aba8a82
emilmont <emilmont@localhost>
2012-10-23 09:33:36 +0000

    Update mbed library

--- main.cpp
+++ main.cpp
@@ -4,18 +4,17 @@ PwmOut r (LED_RED);
 PwmOut g (LED_GREEN);
 PwmOut b (LED_BLUE);
 
-int main()
-{
+int main() {
     r.period(0.001);
     g.period(0.001);
     b.period(0.001);
 
-    while(1) {
-        for(float i = 0.0; i < 1.0 ; i += 0.001) {
+    while (true) {
+        for (float i = 0.0; i < 1.0 ; i += 0.001) {
             float p = 3 * i;
             r = 1.0 - ((p < 1.0) ? 1.0 - p : (p > 2.0) ? p - 2.0 : 0.0);
             g = 1.0 - ((p < 1.0) ? p : (p > 2.0) ? 0.0 : 2.0 - p);
-            b = 1.0 - ((p < 1.0) ? 0.0 : (p > 2.0) ? 3.0 - p : p - 1.0);  ;  
+            b = 1.0 - ((p < 1.0) ? 0.0 : (p > 2.0) ? 3.0 - p : p - 1.0);
             wait (0.0025);
         }
     }
